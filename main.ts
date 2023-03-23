26c64e7eebd12a2c39b446ceabfcbc35fa3e5d59
Keirafranco <125355714+Keirafranco@users.noreply.github.com>
2023-03-23 09:32:04 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 input.onButtonPressed(Button.A, function () {
     radio.sendString("Holaaa ")
 })
-input.onSound(DetectedSound.Loud, function () {
-    radio.sendString("Estoy bien ")
-})
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
+})
+input.onSound(DetectedSound.Loud, function () {
+    radio.sendString("Estoy bien ")
 })
 radio.setGroup(1)
 input.setSoundThreshold(SoundThreshold.Loud, 191)
